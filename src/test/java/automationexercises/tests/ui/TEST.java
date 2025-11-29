f8af663846f4323573b6fcd821ab6b7918fb4640
aya-ali-99 <ayaali969@gmail.com>
2025-11-29 23:50:38 +0200

    Add UI login and register test cases with Allure annotations and data-driven approach

--- src/test/java/automationexercises/tests/ui/TEST.java
+++ src/test/java/automationexercises/tests/ui/TEST.java
@@ -6,17 +6,16 @@ import automationexercises.utils.TimeManager;
 import automationexercises.utils.dataReader.JsonReader;
 import io.qameta.allure.*;
 
-import org.junit.jupiter.api.Test;
 import automationexercises.base.BaseTest;
 
 import automationexercises.pages.*;
+import org.junit.jupiter.api.Test;
 
 
 public class TEST extends BaseTest {
 
-    JsonReader testData = new JsonReader("register-data");
-
     String timestamp = TimeManager.getSimpleTimestamp();
+    JsonReader testData = new JsonReader("register-data");
 
     @Test
     @Story("UI Verifications")
@@ -24,81 +23,10 @@ public class TEST extends BaseTest {
     @Severity(SeverityLevel.CRITICAL)
     @Owner("Aya")
     public void testCaseForVerifications() {
-        new NavigationBarComponent(page).navigate()
-                .clickHomeButton()
-                .verifyHomePageVisible();
+//        new NavigationBarComponent(page).navigate()
+//                .clickOnHomeButton()
+//                .verifyHomePage();
     }
-
-
-
-    @Test
-    public void testCaseForSignUpLoginPage(){
-        new UserManagementAPI().createRegisterUserAccount(
-                testData.getJsonData("name"),
-                testData.getJsonData("email") + timestamp + "@gmail.com",
-                testData.getJsonData("password"),
-                testData.getJsonData("firstName"),
-                testData.getJsonData("lastName")
-        ).verifyUserCreatedSuccessfully();
-
-        new SignupLoginPage(page).navigate()
-                .enterLoginEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
-                .enterLoginPassword(testData.getJsonData("password"))
-                .clickLoginButton()
-                .navigationBar
-                .verifyLoggedInUserName(testData.getJsonData("name"))
-                .clickOnLogoutButton()
-                .navigationBar
-                .verifyLogoutButtonNotVisible()
-                .verifySignupLabelVisible();
-
-        new UserManagementAPI().deleteUserAccount(
-                        testData.getJsonData("email") + timestamp + "@gmail.com",
-                        testData.getJsonData("password"))
-                .verifyUserDeletedSuccessfully();
-
-    }
-
-
-    @Test
-    public void testCaseForRegisterPage(){
-
-        new SignupLoginPage(page).navigate()
-                .enterSignupName(testData.getJsonData("name"))
-                .enterSignupEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
-                .clickSignupButton();
-
-        new SignupPage(page)
-                .chooseTitle(testData.getJsonData("titleMale"))
-                .enterPassword(testData.getJsonData("password"))
-                .selectDateOfBirth(
-                        testData.getJsonData("day"),
-                        testData.getJsonData("month"),
-                        testData.getJsonData("year")
-                )
-                .subscribeToNewsletter()
-                .receiveSpecialOffers()
-                .enterFirstName(testData.getJsonData("firstName"))
-                .enterLastName(testData.getJsonData("lastName"))
-                .enterCompany(testData.getJsonData("companyName"))
-                .enterAddress1(testData.getJsonData("address1"))
-                .enterAddress2(testData.getJsonData("address2"))
-                .selectCountry(testData.getJsonData("country"))
-                .enterState(testData.getJsonData("state"))
-                .enterCity(testData.getJsonData("city"))
-                .enterZipcode(testData.getJsonData("zipcode"))
-                .enterMobileNumber(testData.getJsonData("mobileNumber"))
-                .clickCreateAccountButton()
-                .verifyAccountCreated();
-
-        new UserManagementAPI().deleteUserAccount(
-                        testData.getJsonData("email") + timestamp + "@gmail.com",
-                        testData.getJsonData("password"))
-                .verifyUserDeletedSuccessfully();
-
-
-    }
-
 
 
 }
