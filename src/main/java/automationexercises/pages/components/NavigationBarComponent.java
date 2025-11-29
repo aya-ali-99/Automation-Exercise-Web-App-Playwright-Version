19be8cfae083d892e469622de81bacdca90510dc
aya-ali-99 <ayaali969@gmail.com>
2025-11-29 22:02:05 +0200

    Remove PageActions utility class and refactor NavigationBarComponent to use direct Playwright locators and actions

--- src/main/java/automationexercises/pages/components/NavigationBarComponent.java
+++ src/main/java/automationexercises/pages/components/NavigationBarComponent.java
@@ -2,12 +2,10 @@ package automationexercises.pages.components;
 
 
 import automationexercises.pages.*;
-import automationexercises.utils.actions.PageActions;
 import automationexercises.utils.dataReader.PropertyReader;
 import automationexercises.utils.logs.LogsManager;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.AriaRole;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 
@@ -15,115 +13,120 @@ import static org.junit.Assert.assertThat;
 
 public class NavigationBarComponent {
 
-    private final Page page;
-    PageActions pageActions ;
+    private Page page;
 
     public NavigationBarComponent(Page page) {
         this.page = page;
-        this.pageActions= new PageActions(page);
     }
 
     // Locators (Playwright)
-    private final String homeButton = "//a[.=' Home']";
-    private final String productsButton = "a[href='/products']";
-    private final String cartButton = "//a[.=' Cart']";
-    private final String logoutButton = "//a[.=' Logout']";
-    private final String signupLoginButton = "//a[.=' Signup / Login']";
-    private final String testCasesButton = "//a[.=' Test Cases']";
-    private final String deleteAccountButton = "//a[.=' Delete Account']";
-    private final String apiButton = "//a[.=' API Testing']";
-    private final String videoTutorials = "//a[.=' Video Tutorials']";
-    private final String contactUsButton = "//a[.=' Contact us']";
-    private final String homePageLabel = "FEATURES ITEMS";
-    private final String userLabel = "b";
+    private final Locator homeButton = page.locator("//a[.=' Home']");
 
-    // Utility: Get Locator quickly
+    private final Locator productsButton = page.locator("a[href='/products']");
+
+    private final Locator cartButton = page.locator("//a[.=' Cart']");
+
+    private final Locator logoutButton = page.locator("//a[.=' Logout']");
+
+    private final Locator signupLoginButton = page.locator("//a[.=' Signup / Login']");
+
+    private final Locator testCasesButton = page.locator("//a[.=' Test Cases']");
+
+    private final Locator deleteAccountButton = page.locator("//a[.=' Delete Account']");
+
+    private final Locator apiButton = page.locator("//a[.=' API Testing']");
+
+    private final Locator videoTutorials = page.locator("//a[.=' Video Tutorials']");
+
+    private final Locator contactUsButton = page.locator("//a[.=' Contact us']");
+
+    private final Locator homePageLabel = page.getByText("FEATURES ITEMS");
+
+    private final Locator userLabel = page.locator("b");
 
 
 
-    // ================================
     // Actions
-    // ================================
 
-    @Step("Navigate to the web application")
-    public NavigationBarComponent navigate() {
+    @Step("Navigate to the Home Page")
+    public NavigationBarComponent navigate(){
         page.navigate(PropertyReader.getProperty("baseUrlWeb"));
         return this;
     }
 
-    @Step("Click on Home button")
-    public NavigationBarComponent clickHomeButton() {
-        pageActions.find(homeButton).click();
+    @Step("Click on Home Button")
+    public NavigationBarComponent clickOnHomeButton(){
+        homeButton.click();
         return this;
     }
 
-    @Step("Click on Products button")
-    public ProductsPage clickProductsButton() {
-        pageActions.find(productsButton).click();
+    @Step("Click on Products Button")
+    public ProductsPage clickOnProductsButton(){
+        productsButton.click();
         return new ProductsPage(page);
     }
 
-    @Step("Click on Cart button")
+    @Step("Click on Cart Button")
     public CartPage clickOnCartButton() {
-        pageActions.find(cartButton).click();
+        cartButton.click();
         return new CartPage(page);
     }
 
-    @Step("Click on Logout button")
-    public SignupLoginPage clickOnLogoutButton() {
-        pageActions.find(logoutButton).click();
+    @Step("Click on Logout Button")
+    public SignupLoginPage clickOnLogoutButton(){
+        logoutButton.click();
         return new SignupLoginPage(page);
     }
 
     @Step("Click on Signup/Login Button")
-    public SignupLoginPage clickOnSignupLoginButton() {
-        pageActions.find(signupLoginButton).click();
+    public SignupLoginPage clickOnSignupLoginButton(){
+        signupLoginButton.click();
         return new SignupLoginPage(page);
     }
 
-    @Step("Click on Test Cases button")
-    public TestCasesPage clickOnTestCasesButton() {
-        pageActions.find(testCasesButton).click();
+    @Step("Click on Test Cases Button")
+    public TestCasesPage clickOnTestCasesButton(){
+        testCasesButton.click();
         return new TestCasesPage(page);
     }
 
-    @Step("Click on Delete Account button")
-    public DeleteAccountPage clickOnDeleteAccountButton() {
-        pageActions.find(deleteAccountButton).click();
+    @Step("Click on Delete Account Button")
+    public DeleteAccountPage clickOnDeleteAccountButton(){
+        deleteAccountButton.click();
         return new DeleteAccountPage(page);
     }
 
-    @Step("Click on Contact Us button")
-    public ContactUsPage clickOnContactUsButton() {
-        pageActions.find(contactUsButton).click();
+    @Step("Click on ContactUs Button")
+    public ContactUsPage clickOnContactUsButton(){
+        contactUsButton.click();
         return new ContactUsPage(page);
     }
 
-    // ================================
+
     // Validations
-    // ================================
+
+    @Step("Verify Home Page Label")
+    public NavigationBarComponent verifyHomePage(){
+        Assertions.assertTrue(homePageLabel.isVisible(),
+                "The Home Page label is not visible after navigation.");
+        return this;
+    }
+
+    @Step("Verify User is Logged in")
+    public NavigationBarComponent verifyUserLabel(String expectedUser){
+        String actualName = userLabel.textContent();
+        LogsManager.info("Verifying logged in user label. Actual: " + actualName
+                + " | Expected: Logged in as " + expectedUser);
+        Assertions.assertEquals(expectedUser, actualName,
+                "Logged in user label does not match expected value. Expected: "
+                        + expectedUser + ", Actual: " + actualName);
+        return this;
+    }
 
     @Step("Verify that user is logged out into Login Page")
     public SignupLoginPage verifyLogoutButtonNotVisible() {
-
-        Assertions.assertTrue(pageActions.find(logoutButton).isHidden(), "The Logout button is unexpectedly visible, suggesting the user is still logged in.");
+        Assertions.assertTrue(logoutButton.isHidden(),
+                "The Logout button is not visible.");
         return new SignupLoginPage(page);
-    }
-
-    @Step("Verify that Home Page is visible successfully")
-    public NavigationBarComponent verifyHomePageVisible() {
-
-        Assertions.assertTrue(pageActions.findByText(homePageLabel).isVisible(), "The Home Page label is not visible after navigation.");
-        return this;
-    }
-
-    @Step("Verify that logged in as {username} is visible")
-    public NavigationBarComponent verifyLoggedInUserName(String username) {
-
-        String actualName = pageActions.find(userLabel).textContent();
-        LogsManager.info("Verifying logged in user label. Actual: " + actualName + " | Expected: Logged in as " + username);
-        Assertions.assertEquals(username, actualName,
-                "Logged in user label does not match expected value. Expected: " + username + " | Actual: " + actualName);
-        return this;
     }
 }
