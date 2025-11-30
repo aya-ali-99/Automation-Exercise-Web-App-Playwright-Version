8a9b3883a16e8694c37eaa5568dbac05e98761f9
aya-ali-99 <ayaali969@gmail.com>
2025-11-30 17:14:24 +0200

    Add ProductDetailsPage functionality and UI tests for product details and reviews

--- src/main/java/automationexercises/pages/ProductDetailsPage.java
+++ src/main/java/automationexercises/pages/ProductDetailsPage.java
@@ -1,14 +1,104 @@
 package automationexercises.pages;
 
-
+import automationexercises.utils.dataReader.PropertyReader;
+import automationexercises.utils.logs.LogsManager;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
 
 public class ProductDetailsPage {
 
-    private final Page page;
+    private Page page;
+
+    // Variables
+    private String productDetailsPage = "/product_details/1";
+
+    // Locators
+    private final Locator productName;
+    private final Locator productPrice;
+    private final Locator addToCartButton;
+    private final Locator quantityInput;
+    private final Locator reviewName;
+    private final Locator reviewEmail;
+    private final Locator reviewTextArea;
+    private final Locator submitReviewButton;
+    private final Locator reviewSuccessMessage;
+    private final Locator viewCartBtn;
 
     public ProductDetailsPage(Page page) {
         this.page = page;
+
+        // Initialize locators
+        this.productName = page.locator(".product-information h2");
+        this.productPrice = page.locator(".product-information span span");
+        this.addToCartButton = page.locator(".product-information button");
+        this.quantityInput = page.locator("#quantity");
+        this.reviewName = page.locator("#name");
+        this.reviewEmail = page.locator("#email");
+        this.reviewTextArea = page.getByPlaceholder("Add Review Here!");
+        this.submitReviewButton = page.locator("//*[@id='button-review']");
+        this.reviewSuccessMessage = page.locator(".alert-success span");
+        this.viewCartBtn = page.locator(".modal-content u");
+    }
+
+    // Actions
+
+    @Step("Navigate to products details")
+    public ProductDetailsPage navigate() {
+        page.navigate(PropertyReader.getProperty("baseUrlWeb") + productDetailsPage);
+        LogsManager.info("Navigated to product details page");
+        return this;
+    }
+
+    @Step("Add product to cart")
+    public ProductDetailsPage clickOnAddToCartButton(){
+        addToCartButton.click();
+        return this;
+    }
+
+    @Step("Add Review")
+    public ProductDetailsPage addReview(String name, String email, String review){
+        reviewName.fill(name);
+        reviewEmail.fill(email);
+        reviewTextArea.fill(review);
+        submitReviewButton.click();
+        return this;
+    }
+
+    @Step("Add quantity")
+    public ProductDetailsPage addQuantity(String quantity){
+        quantityInput.fill(quantity);
+        return this;
+    }
+
+    @Step("Click on view cart button")
+    public CartPage clickOnViewCartButton(){
+        viewCartBtn.click();
+        return new CartPage(page);
+    }
+
+    // Verifications
+    @Step("Verify product details")
+    public ProductDetailsPage validateProductDetails(String productName, String productPrice){
+        String actualProductName = this.productName.textContent();
+        String actualProductPrice = this.productPrice.textContent();
+        LogsManager.info("Actual product name: " + actualProductName +
+                ", actual price: " + actualProductPrice);
+        Assertions.assertEquals(productName, actualProductName,
+                "Product name does not match");
+        Assertions.assertEquals(productPrice, actualProductPrice,
+                "Product price does not match");
+        return this;
+    }
+
+    @Step("Verify review success message")
+    public ProductDetailsPage validateReviewSuccessMessage(String SuccessMessage){
+        String actualSuccessMessage = reviewSuccessMessage.textContent();
+        LogsManager.info("Actual review success message: " + actualSuccessMessage);
+        Assertions.assertEquals(SuccessMessage, actualSuccessMessage,
+                "Review success message does not match");
+        return this;
     }
 
 }
---
+++ src/test/java/automationexercises/tests/ui/ProductDetailsTest.java
@@ -0,0 +1,59 @@
+package automationexercises.tests.ui;
+
+import automationexercises.base.BaseTest;
+import automationexercises.pages.ProductsPage;
+import automationexercises.utils.dataReader.JsonReader;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Test;
+
+
+
+@Epic("Automation Exercise ")
+@Feature("UI Products Management")
+@Story("Product Details")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("Aya")
+public class ProductDetailsTest extends BaseTest {
+    JsonReader testData = new JsonReader("product-details-data");
+
+    // Tests
+
+    @Test
+    @Description("Verify product details")
+    public void verifyProductDetailsWithoutLoginTC(){
+        new ProductsPage(page)
+                .navigate()
+                .clickOnViewProduct(testData.getJsonData("product.name"))
+                .validateProductDetails(
+                        testData.getJsonData("product.name"),
+                        testData.getJsonData("product.price"));
+    }
+
+
+    @Test
+    @Description("Verify product review success message")
+    public void verifyProductReviewSuccessMessageWithoutLoginTC(){
+        new ProductsPage(page)
+                .navigate()
+                .clickOnViewProduct(testData.getJsonData("product.name"))
+                .addReview(
+                        testData.getJsonData("review.name"),
+                        testData.getJsonData("review.email"),
+                        testData.getJsonData("review.review"))
+                .validateReviewSuccessMessage(testData.getJsonData("messages.review"));
+    }
+
+//    @Test
+//    @Description("Verify product quantity in cart")
+//    public void verifyProductQuantityInCartWithoutLoginTC(){
+//        new ProductsPage(page)
+//                .navigate()
+//                .clickOnViewProduct(testData.getJsonData("product.name"))
+//                .addQuantity(testData.getJsonData("addQuantity"))
+//                .clickOnAddToCartButton()
+//                .clickOnViewCartButton()
+//                .verifyProductQuantityInCart(testData.getJsonData("product.name"),
+//                        testData.getJsonData("addQuantity"));
+//    }
+
+}
